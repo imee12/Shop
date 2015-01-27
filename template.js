7870fdf0d271caf785f7cf343acdd7506f42b701
Imee Cuison <imeetwelve@gmail.com>
2015-01-27 11:48:52 -0500

    form made

---
+++ template.js
@@ -0,0 +1,20 @@
+var templates = ();
+
+templates.product = [
+  "<article>",
+  "<h2><%= name %></h2>"
+  "<img><%= image %></img>"
+  "<p><%= description %></p>"
+  "<blockquote><%= price %><blockquote>"
+  "</article>"
+].join ("");
+
+templates.sidebar = [
+  "<article>",
+  "<h2><%= name %></h2>"
+  "<img><%= image %></img>"
+  "<p><%= description %></p>"
+  "<blockquote><%= price %><blockquote>"
+  "</article>"
+
+].join("");
